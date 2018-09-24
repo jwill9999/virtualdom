ef3668424d6e0f17c2dd49c75e4461dde727b3a1
Jason Williams <jasonwilliams2308@gmail.com>
2018-09-24 14:03:46 +0100

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,46 @@
 module.exports = {
-    "extends": "airbnb"
+    
+    "env": {
+        "mocha": true
+    },
+    "extends":[ 
+        "airbnb-base",
+        "prettier",
+        "prettier/react"
+        ],
+        "plugins": [   
+            "import",
+            "react",        
+            "prettier",
+            "jsx-a11y"  
+          ],
+        "rules": {
+           "prettier/prettier": "error",
+           "linebreak-style": ["error", "windows"],
+           "no-console": 0,
+           "radix": 0,
+           "no-param-reassign": 0, 
+           "react/jsx-filename-extension": [
+            1,
+            {
+              "extensions": [
+                ".js",
+                ".jsx"
+              ]
+            }
+          ],
+          "react/jsx-uses-react": "error",
+          "react/jsx-uses-vars": "error",          
+           
+        },
+        "overrides": [
+            {
+                "files": ["*.spec.js", "*.test.js"],
+                "rules": {
+                    "no-unused-expressions": 0
+                }
+            }
+        ]
+   
+  
 };
